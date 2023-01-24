137e371eac09a34afa495e3f6bcb8787de584fae
QuangDo <do.quang03@gmail.com>
2023-01-24 11:53:25 +0700

    Add layout register

---
+++ src/component/RegisterHeader/index.ts
@@ -0,0 +1,1 @@
+export { default as RegisterHeader } from './RegisterHeader';
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary10: '#ee4d2d'
+      }
+    }
   },
   plugins: []
 };
