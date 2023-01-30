fead8a256372bd5711fb27ffa0b26d628491718e
QuangDo <62138506+QuangDoo@users.noreply.github.com>
2023-01-30 11:56:55 +0700

    add aside bar (#16)
    Co-authored-by: QuangDo <do.quang03@gmail.com>

--- src/component/Input/Input.tsx
+++ src/component/Input/Input.tsx
@@ -1,16 +1,15 @@
+import { InputHTMLAttributes } from 'react';
 import type { RegisterOptions, UseFormRegister } from 'react-hook-form';
 
 type Props = {
-  type?: React.HTMLInputTypeAttribute;
-  placeholder?: string;
   containerClassName?: string;
   inputClassName?: string;
-  name: string;
-  register: UseFormRegister<any>;
+  classNameError?: string;
+  register?: UseFormRegister<any>;
   rules?: RegisterOptions;
   errorMessage?: string;
   autoComplete?: string;
-};
+} & InputHTMLAttributes<HTMLInputElement>;
 
 const Input = (props: Props) => {
   const {
@@ -21,20 +20,23 @@ const Input = (props: Props) => {
     name,
     rules,
     errorMessage,
-    inputClassName,
+    inputClassName = 'w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm',
+    classNameError = 'mt-1 min-h-[1.25rem] text-sm text-red-600',
     autoComplete
   } = props;
+
+  const registerResult = register && name ? register(name, rules) : {};
 
   return (
     <div className={containerClassName}>
       <input
-        {...register(name, rules)}
+        {...registerResult}
         type={type}
-        className={`w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm ${inputClassName}`}
+        className={inputClassName}
         placeholder={placeholder}
         autoComplete={autoComplete}
       />
-      <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errorMessage}</div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   );
 };
--- src/pages/ProductList/index.ts
+++ src/pages/ProductList/index.ts
@@ -1,1 +1,2 @@
 export { default as ProductList } from './ProductList';
+export * from './components';
